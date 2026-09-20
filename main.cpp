5742df599b726480aad50e3e3e5f695f48aa8176
polll1k <solomkopolina6@gmail.com>
2026-09-20 23:44:46 +0300

    ввод трубы

--- main.cpp
+++ main.cpp
@@ -3,20 +3,35 @@
 using namespace std;
 
 struct Pipe{
-    string name;
-    double length;
-    int diameter;
-    bool inRepair;
+ string name;
+ double length;
+ int diameter;
+ bool inRepair;
 };
 
 struct CompressorStation{
-    string name;
-    int workshops;
-    int workshopsInWork;
-    int stationClass;
+ string name;
+ int workshops;
+ int workshopsInWork;
+ int stationClass;
 };
 
+void addPipe(Pipe& p){
+ cout<<"Название трубы: ";
+ cin>>p.name;
+ cout<<"Длина трубы (км): ";
+ cin>>p.length;
+ cout<<"Диаметр трубы (мм): ";
+ cin>>p.diameter;
+ cout<<"Труба в ремонте? (1-да, 0-нет): ";
+ cin>>p.inRepair;
+}
+
 int main(){
-    cout<<"Программа для работы с трубой и КС"<<endl;
-    return 0;
+ Pipe pipe;
+
+ cout<<"Программа для работы с трубой и КС"<<endl;
+ addPipe(pipe);
+
+ return 0;
 }
